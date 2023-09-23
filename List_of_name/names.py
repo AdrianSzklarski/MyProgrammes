5d6f3623bb26be75c421c1ad7583a28eb460fe0e
AdrianSzklarski <adrian_it@o2.pl>
2023-09-23 23:04:41 +0200

    list of name

--- List_of_name/names.py
+++ List_of_name/names.py
@@ -1,6 +1,9 @@
 '''
 Run file:
-python3 names.py > index.html & firefox index.html
+ls
+python3 names.py > index.html - writing code from Python to html
+python3 names.py > index.html & firefox index.html - run app
+cat index.html - display in the console the contents of the index.html file
 '''
 
 link = r'/home/adrian/Pulpit/GitHub/My Programmes/List_of_name/Names.txt'
@@ -21,15 +24,15 @@ for lines in f:
 f.close()
 
 #  ver.2
-with open(link, 'r', encoding='UTF-8') as f:
-    for lines in f:
-        lines = lines.strip()
-        name = lines.split(',')[0]
-        print(f'''
-        <html>
-            <body>
-                <h1>The following people are invited to 12:00 pm: {name}</h1>
-                <p>Best Regards, Your Boss</p>
-            </body>
-        </html>
-        ''')
+# with open(link, 'r', encoding='UTF-8') as f:
+#     for lines in f:
+#         lines = lines.strip()
+#         name = lines.split(',')[0]
+#         print(f'''
+#         <html>
+#             <body>
+#                 <h1>The following people are invited to 12:00 pm: {name}</h1>
+#                 <p>Best Regards, Your Boss</p>
+#             </body>
+#         </html>
+#         ''')
